7bcd4a61025dd8e27186c3129a1922413f72dd1b
sayudha <mazaya.studio@outlook.com>
2022-11-03 10:27:49 +0700

    add loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// show loading
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
+
 // show new quote
 function newQuote() {
+	loading();
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 	quote.author
 			? authorText.textContent = quote.author
@@ -16,6 +30,7 @@ function newQuote() {
 			? quoteText.classList.add('long-quote')
 			: quoteText.classList.remove('long-quote');
 	quoteText.textContent = quote.text;
+	complete();
 }
 
 // get quotes from api
