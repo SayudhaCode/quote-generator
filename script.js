48bb4a3331dedce8d79bf45a0aea87be85bcd567
sayudha <mazaya.studio@outlook.com>
2022-11-03 07:53:10 +0700

    DOM Manipulation

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // show new quote
 function newQuote() {
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote);
+	quote.author
+			? authorText.textContent = quote.author
+			: authorText.textContent = 'Unknown';
+	quote.text.length > 120
+			? quoteText.classList.add('long-quote')
+			: quoteText.classList.remove('long-quote');
+	quoteText.textContent = quote.text;
 }
 
 // get quotes from api
@@ -17,6 +29,16 @@ async function getQuotes() {
 		console.log(error);
 	}
 }
+
+// tweet quote
+function tweetQuote() {
+	const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterURL, '_blank');
+}
+
+// event listener
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // on load
 getQuotes();
